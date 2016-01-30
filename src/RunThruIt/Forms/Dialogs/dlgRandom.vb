c9a4a847e3866bd6fddefe05a286b46dcda91800
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 02:23:56 -0500

    Outline dlgRandom code

--- src/RunThruIt/Forms/Dialogs/dlgRandom.vb
+++ src/RunThruIt/Forms/Dialogs/dlgRandom.vb
@@ -1,3 +1,27 @@
 ﻿Public Class dlgRandom
 
+#Region "UI"
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+
+    End Sub
+
+    Private Sub btnNewQuote_Click(sender As Object, e As EventArgs) Handles btnNewQuote.Click
+
+    End Sub
+
+    Private Sub btnNewTip_Click(sender As Object, e As EventArgs) Handles btnNewTip.Click
+
+    End Sub
+
+#End Region
+
+#Region "dlgRandom"
+
+    Private Sub dlgRandom_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+#End Region
+    
 End Class
