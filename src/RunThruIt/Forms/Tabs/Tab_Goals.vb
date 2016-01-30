38a70dfc23e60d1f86f4c36cd4b170191302fe41
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 11:45:30 -0500

    Implement Mark Goal as completed toolstrip menu item

--- src/RunThruIt/Forms/Tabs/Tab_Goals.vb
+++ src/RunThruIt/Forms/Tabs/Tab_Goals.vb
@@ -11,7 +11,7 @@
     End Sub
 
     Private Sub MarkAsCompletedToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MarkAsCompletedToolStripMenuItem.Click
-
+        MarkGoalAsCompleted()
     End Sub
 
     Private Sub AddGoalToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddGoalToolStripMenuItem.Click
