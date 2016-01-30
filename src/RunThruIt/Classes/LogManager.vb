a6b54bf6ab427e3daf467ac4d8df626eaf86716e
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 10:12:36 -0500

    Organize convert methods in region, rename ParseSetting to ConvertStringToWorkout

--- src/RunThruIt/Classes/LogManager.vb
+++ src/RunThruIt/Classes/LogManager.vb
@@ -18,20 +18,19 @@
 
     End Function
 
-
     Public Shared Sub ClearWorkouts()
         My.Settings.log_workout.Clear()
     End Sub
+
+#End Region
+
+#Region "Convert"
 
     Public Shared Function ConvertWorkoutToString(ByVal nworkout As Workout) As String
         Return nworkout.WorkoutType & "|" & nworkout.WorkoutDate.ToString() & "|" & nworkout.Notes & "|" & nworkout.Distance & "|" & nworkout.Shoes & "|" & nworkout.Pace & "|" & nworkout.Injured.ToString
     End Function
 
-#End Region
-
-#Region "Private"
-
-    Private Function ParseSetting(ByVal str As String) As Workout
+    Public Shared Function ConvertStringToWorkout(ByVal str As String) As Workout
         Dim arr() As String = str.Split("|")
         Return New Workout(arr(0), Date.Parse(arr(1)), arr(2), arr(3), arr(4), arr(5), Boolean.Parse(arr(6)))
     End Function
