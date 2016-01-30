75f8e9d21c3fbfac272c4410b54ee3121a0c1f54
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 09:59:34 -0500

    Add constructors to Workout class

--- src/RunThruIt/Classes/Workout.vb
+++ src/RunThruIt/Classes/Workout.vb
@@ -8,4 +8,22 @@
     Public Property Pace As String
     Public Property Injured As Boolean
 
+#Region "Constructor"
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(ByVal wtype As String, ByVal wdate As Date, ByVal wnotes As String, ByVal wdistance As String, ByVal wshoes As String, ByVal wpace As String, ByVal winjured As Boolean)
+        WorkoutType = wtype
+        WorkoutDate = wdate
+        Notes = wnotes
+        Distance = wdistance
+        Shoes = wshoes
+        Pace = wpace
+        Injured = winjured
+    End Sub
+
+#End Region
+
 End Class
