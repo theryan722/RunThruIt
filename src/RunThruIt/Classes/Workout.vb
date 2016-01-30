958e4b69cb0133163f678ff20b2a75fdaf8c6b51
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 23:26:29 -0500

    Workout Object

---
+++ src/RunThruIt/Classes/Workout.vb
@@ -0,0 +1,10 @@
+﻿Public Class Workout
+
+    Public Property WorkoutType As String
+    Public Property WorkoutDateTime As DateTime
+    Public Property WorkoutNotes As String
+    Public Property WorkoutDistance As String
+    Public Property Shoes As String
+    Public Property Goal As String
+
+End Class
