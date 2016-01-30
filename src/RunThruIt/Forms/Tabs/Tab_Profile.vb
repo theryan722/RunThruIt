3c53810213ae1eb1d47035b69cc507af75b328d5
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 03:03:32 -0500

    Improve LoadProfile()

--- src/RunThruIt/Forms/Tabs/Tab_Profile.vb
+++ src/RunThruIt/Forms/Tabs/Tab_Profile.vb
@@ -10,11 +10,11 @@
 
     Private Sub LoadProfile()
         lbl_name.Text = "Name: " & My.Settings.profile_name
-        lbl_age.Text = "Age: " & My.Settings.profile_age
+        lbl_age.Text = If(My.Settings.profile_age = "", "Age: Unknown", "Age: " & My.Settings.profile_age)
         lbl_sex.Text = "Sex: " & My.Settings.profile_sex
-        lbl_activitylevel.Text = "Activity Level: " & My.Settings.profile_activitylevel
-        lbl_weight.Text = "Weight: " & My.Settings.profile_weight
-        pb_profilepicture.BackgroundImage = Image.FromFile(My.Settings.profile_picture)
+        lbl_activitylevel.Text = If(My.Settings.profile_activitylevel = "", "Activity Level: Unknown", "Activity Level: " & My.Settings.profile_activitylevel)
+        lbl_weight.Text = If(My.Settings.profile_weight = "", "Weight: Unknown", "Weight: " & My.Settings.profile_weight)
+        pb_profilepicture.BackgroundImage = If(My.Settings.profile_picture = "", My.Resources.profile_128, Image.FromFile(My.Settings.profile_picture))
     End Sub
 
 #End Region
