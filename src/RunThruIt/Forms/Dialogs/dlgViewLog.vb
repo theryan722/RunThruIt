b6f18dfd30d4c2264bfe5fea7a6c228bb71ac59d
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 10:34:46 -0500

    Implement btnClose

--- src/RunThruIt/Forms/Dialogs/dlgViewLog.vb
+++ src/RunThruIt/Forms/Dialogs/dlgViewLog.vb
@@ -1,3 +1,12 @@
 ﻿Public Class dlgViewLog
 
+#Region "UI"
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+#End Region
+
+    
 End Class
