faf2a1af629462a936451741632777720afbe597
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 10:35:56 -0500

    Outline rest of dlgViewLog code

--- src/RunThruIt/Forms/Dialogs/dlgViewLog.vb
+++ src/RunThruIt/Forms/Dialogs/dlgViewLog.vb
@@ -8,5 +8,24 @@
 
 #End Region
 
+#Region "Methods"
+
+    Public Sub DisplayWorkout(ByVal nworkout As Workout)
+
+    End Sub
+
+#End Region
+
+#Region "dlgViewLog"
+
+    Private Sub dlgViewLog_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+
+    End Sub
+
+    Private Sub dlgViewLog_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+#End Region
     
 End Class
