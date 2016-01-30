6922479e2555bffdd9b00358f8ffaa1d308d26eb
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 22:40:42 -0500

    Tab_Options Properties

--- src/RunThruIt/Forms/Tabs/Tab_Options.Designer.vb
+++ src/RunThruIt/Forms/Tabs/Tab_Options.Designer.vb
@@ -22,8 +22,18 @@ Partial Class Tab_Options
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'Tab_Options
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Tab_Options"
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Tab_Options"
+        Me.ShowInTaskbar = False
+        Me.Text = "Options"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
