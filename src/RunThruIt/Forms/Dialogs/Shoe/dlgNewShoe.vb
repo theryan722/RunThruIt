c8a6a7d0c9589b32a78a68f670b42438f0cfb626
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 14:11:04 -0500

    Code outline, Implement CreateShoe()

--- src/RunThruIt/Forms/Dialogs/Shoe/dlgNewShoe.vb
+++ src/RunThruIt/Forms/Dialogs/Shoe/dlgNewShoe.vb
@@ -8,17 +8,29 @@
         Me.DialogResult = Windows.Forms.DialogResult.Cancel
     End Sub
 
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+
+    End Sub
+
 #End Region
 
 #Region "Methods"
 
+    Private Function VerifyFields() As Boolean
 
+    End Function
+
+    Private Sub CreateShoe(ByVal sname As String, ByVal smileage As String, ByVal smaxmileage As String, ByVal snotes As String, ByVal sdate As Date)
+        NewShoe = New Shoe(sname, smileage, snotes, sdate, smaxmileage)
+    End Sub
 
 #End Region
 
 #Region "dlgNewShoe"
 
+    Private Sub dlgNewShoe_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
 
 #End Region
 
