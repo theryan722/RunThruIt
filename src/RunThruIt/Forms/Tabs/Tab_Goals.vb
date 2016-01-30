a8630f9f23c4abc7645547b64a480d10662df36b
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 11:31:00 -0500

    Add regions

--- src/RunThruIt/Forms/Tabs/Tab_Goals.vb
+++ src/RunThruIt/Forms/Tabs/Tab_Goals.vb
@@ -1,3 +1,39 @@
 ﻿Public Class Tab_Goals
 
+#Region "UI"
+
+#Region "TabGoals"
+
+#Region "Context Menu Strip"
+
+
+
+#End Region
+
+#End Region
+
+#Region "TabCompleted"
+
+#Region "Context Menu Strip"
+
+
+
+#End Region
+
+#End Region
+
+#End Region
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "Tab_Goals"
+
+
+
+#End Region
+
 End Class
