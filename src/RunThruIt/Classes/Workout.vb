a7c693b144cdd23c91b1a988b2d89d01cd7a4065
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 09:38:16 -0500

    Update Workout Properties

--- src/RunThruIt/Classes/Workout.vb
+++ src/RunThruIt/Classes/Workout.vb
@@ -1,10 +1,11 @@
 ﻿Public Class Workout
 
     Public Property WorkoutType As String
-    Public Property WorkoutDateTime As DateTime
-    Public Property WorkoutNotes As String
-    Public Property WorkoutDistance As String
+    Public Property WorkoutDate As Date
+    Public Property Notes As String
+    Public Property Distance As String
     Public Property Shoes As String
-    Public Property Goal As String
+    Public Property Pace As String
+    Public Property Injured As Boolean
 
 End Class
