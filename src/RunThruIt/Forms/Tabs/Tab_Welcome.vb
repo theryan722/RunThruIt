59b886e7cd041bdd6d145309d45395a8195a595c
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 23:12:48 -0500

    Implement Tab_Welcome buttons

--- src/RunThruIt/Forms/Tabs/Tab_Welcome.vb
+++ src/RunThruIt/Forms/Tabs/Tab_Welcome.vb
@@ -1,3 +1,23 @@
 ﻿Public Class Tab_Welcome
 
+#Region "Buttons"
+
+    Private Sub btnProfile_Click(sender As Object, e As EventArgs) Handles btnProfile.Click
+        Tabs.AddProfile()
+    End Sub
+
+    Private Sub btnGoals_Click(sender As Object, e As EventArgs) Handles btnGoals.Click
+        Tabs.AddGoals()
+    End Sub
+
+    Private Sub btnLog_Click(sender As Object, e As EventArgs) Handles btnLog.Click
+        Tabs.AddLog()
+    End Sub
+
+    Private Sub btnInjury_Click(sender As Object, e As EventArgs) Handles btnInjury.Click
+        Tabs.AddInjury()
+    End Sub
+
+#End Region
+
 End Class
