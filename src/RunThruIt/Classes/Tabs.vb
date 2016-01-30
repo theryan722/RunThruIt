6ab8f6ef88a69b549c9a8445002d8119163bc974
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 22:06:05 -0500

    AddGoals()

--- src/RunThruIt/Classes/Tabs.vb
+++ src/RunThruIt/Classes/Tabs.vb
@@ -14,6 +14,18 @@
         frmManager.TabControl1.SelectedTab = t
     End Sub
 
+    Public Shared Sub AddGoals()
+        Dim t As New TabPage
+        Dim newtab As New Tab_Goals
+        newtab.Show()
+        newtab.Dock = DockStyle.Fill
+        newtab.TopLevel = False
+        t.Controls.Add(newtab)
+        frmManager.TabControl1.TabPages.Add(t)
+        newtab.Parent.Text = newtab.Text
+        frmManager.TabControl1.SelectedTab = t
+    End Sub
+
 #End Region
 
 End Class
