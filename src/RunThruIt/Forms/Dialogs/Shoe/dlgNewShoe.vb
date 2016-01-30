bd84611915d6c719d166f6dcebf849ca48abb905
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 14:07:27 -0500

    Implement btnCancel

--- src/RunThruIt/Forms/Dialogs/Shoe/dlgNewShoe.vb
+++ src/RunThruIt/Forms/Dialogs/Shoe/dlgNewShoe.vb
@@ -4,7 +4,9 @@
 
 #Region "UI"
 
-
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Me.DialogResult = Windows.Forms.DialogResult.Cancel
+    End Sub
 
 #End Region
 
