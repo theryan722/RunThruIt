803a058146819c7e4f10473ceb9e4224894d961e
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 13:24:55 -0500

    Large chunk of shoe code

--- src/RunThruIt/Classes/Objects/Shoe.vb
+++ src/RunThruIt/Classes/Objects/Shoe.vb
@@ -4,6 +4,7 @@
     Public Property Mileage As String
     Public Property Notes As String
     Public Property ShoeIsDefault As Boolean
+    Public Property DatePurchased As Date
 
 #Region "Constructor"
 
@@ -11,13 +12,18 @@
 
     End Sub
 
-    Public Sub New(ByVal sname As String, ByVal smileage As String, ByVal snotes As String, ByVal sdef As Boolean)
+    Public Sub New(ByVal sname As String, ByVal smileage As String, ByVal snotes As String, ByVal sdef As Boolean, ByVal sdate As Date)
         ShoeName = sname
         Mileage = smileage
         Notes = snotes
         ShoeIsDefault = sdef
+        DatePurchased = sdate
     End Sub
 
 #End Region
+
+    Public Sub AlertIfHighMileage()
+
+    End Sub
 
 End Class
