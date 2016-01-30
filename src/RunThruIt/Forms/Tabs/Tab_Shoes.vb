1640d4b3a032b645bd57e5d88d7c21eb8fda95d9
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 14:23:21 -0500

    Code Outline

--- src/RunThruIt/Forms/Tabs/Tab_Shoes.vb
+++ src/RunThruIt/Forms/Tabs/Tab_Shoes.vb
@@ -1,3 +1,39 @@
 ﻿Public Class Tab_shoes
 
+#Region "UI"
+
+    Private Sub btnReload_Click(sender As Object, e As EventArgs) Handles btnReload.Click
+
+    End Sub
+
+    Private Sub btnRemoveAll_Click(sender As Object, e As EventArgs) Handles btnRemoveAll.Click
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+    End Sub
+
+#End Region
+
+#Region "Methods"
+
+    Private Sub LoadShoes()
+
+    End Sub
+
+    Private Sub RemoveAllShoeItems()
+
+    End Sub
+
+#End Region
+
+#Region "Tab_Shoes"
+
+    Private Sub Tab_shoes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+#End Region
+
 End Class
