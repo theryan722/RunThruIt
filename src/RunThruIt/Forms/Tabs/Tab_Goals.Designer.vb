519dd6fbe735439c714ea9a125830d5cb5267142
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 22:44:06 -0500

    Tab_Goals Properties

--- src/RunThruIt/Forms/Tabs/Tab_Goals.Designer.vb
+++ src/RunThruIt/Forms/Tabs/Tab_Goals.Designer.vb
@@ -22,8 +22,18 @@ Partial Class Tab_Goals
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'Tab_Goals
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Tab_Goals"
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Tab_Goals"
+        Me.ShowInTaskbar = False
+        Me.Text = "Goals"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
