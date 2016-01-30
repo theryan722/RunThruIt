bafc1d4ebb37694c2a4ec251965f09a1df9adf1a
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 21:53:41 -0500

    Convert dlgNewLog to MetroForm

--- src/RunThruIt/Forms/Dialogs/dlgNewLog.Designer.vb
+++ src/RunThruIt/Forms/Dialogs/dlgNewLog.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class dlgNewLog
-    Inherits System.Windows.Forms.Form
+    Inherits MetroFramework.Forms.MetroForm
 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
@@ -22,8 +22,17 @@ Partial Class dlgNewLog
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'dlgNewLog
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(300, 300)
+        Me.Name = "dlgNewLog"
+        Me.Padding = New System.Windows.Forms.Padding(0, 60, 0, 20)
         Me.Text = "dlgNewLog"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
