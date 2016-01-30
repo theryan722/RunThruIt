506cdcaec0668993f05564bae1152fb26a0111be
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 02:57:30 -0500

    LoadProfile()

--- src/RunThruIt/Forms/Tabs/Tab_Profile.vb
+++ src/RunThruIt/Forms/Tabs/Tab_Profile.vb
@@ -8,7 +8,14 @@
 
 #Region "Methods"
 
-
+    Private Sub LoadProfile()
+        lbl_name.Text = "Name: " & My.Settings.profile_name
+        lbl_age.Text = "Age: " & My.Settings.profile_age
+        lbl_sex.Text = "Sex: " & My.Settings.profile_sex
+        lbl_activitylevel.Text = "Activity Level: " & My.Settings.profile_activitylevel
+        lbl_weight.Text = "Weight: " & My.Settings.profile_weight
+        pb_profilepicture.BackgroundImage = Image.FromFile(My.Settings.profile_picture)
+    End Sub
 
 #End Region
 
