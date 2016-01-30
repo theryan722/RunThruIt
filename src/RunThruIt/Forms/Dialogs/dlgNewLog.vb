96245c4e0ad2c8bf5ab1787595b0fb6b7b4f86b0
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 09:40:31 -0500

    CreateWorkout()

--- src/RunThruIt/Forms/Dialogs/dlgNewLog.vb
+++ src/RunThruIt/Forms/Dialogs/dlgNewLog.vb
@@ -17,7 +17,15 @@
 #Region "Methods"
 
     Private Sub CreateWorkout(ByVal type As String, ByVal wdate As Date, ByVal notes As String, ByVal distance As String, ByVal shoes As String, ByVal pace As String, ByVal injured As Boolean)
-
+        Dim b As Workout = New Workout
+        b.WorkoutType = type
+        b.WorkoutDate = wdate
+        b.Notes = notes
+        b.Distance = distance
+        b.Shoes = shoes
+        b.Pace = pace
+        b.Injured = injured
+        NewWorkout = b
     End Sub
 
 #End Region
