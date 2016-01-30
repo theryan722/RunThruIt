17330a19ccebded9820535a4c562c6c5413ce029
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 22:16:42 -0500

    frmManager code regions

--- src/RunThruIt/Forms/frmManager.vb
+++ src/RunThruIt/Forms/frmManager.vb
@@ -1,3 +1,37 @@
 ﻿Public Class frmManager
 
+#Region "MenuStrip"
+
+#Region "File"
+
+
+
+#End Region
+
+#Region "Tabs"
+
+
+
+#End Region
+
+#Region "About"
+
+
+
+#End Region
+
+#End Region
+
+#Region "Methods"
+
+
+
+#End Region
+
+#Region "frmManager"
+
+
+
+#End Region
+
 End Class
