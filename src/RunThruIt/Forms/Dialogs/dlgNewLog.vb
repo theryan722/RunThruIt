441635d7044748f537769c4db9acbeafdcf27ca5
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 09:37:00 -0500

    Outline dlgNewLog code

--- src/RunThruIt/Forms/Dialogs/dlgNewLog.vb
+++ src/RunThruIt/Forms/Dialogs/dlgNewLog.vb
@@ -1,3 +1,25 @@
 ﻿Public Class dlgNewLog
 
+    Public Property NewWorkout As Workout
+
+#Region "UI"
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+
+    End Sub
+
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+
+    End Sub
+
+#End Region
+
+#Region "Methods"
+
+    Private Sub CreateWorkout(ByVal type As String, ByVal wdate As Date, ByVal notes As String, ByVal distance As String, ByVal shoes As String, ByVal pace As String, ByVal injured As Boolean)
+
+    End Sub
+
+#End Region
+
 End Class
