9335e4af006c2090c63c94b8117405b2f04fd222
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 22:40:04 -0500

    Tab_Profile Properties

--- src/RunThruIt/Forms/Tabs/Tab_Profile.Designer.vb
+++ src/RunThruIt/Forms/Tabs/Tab_Profile.Designer.vb
@@ -22,8 +22,18 @@ Partial Class Tab_Profile
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'Tab_Profile
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Tab_Profile"
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Tab_Profile"
+        Me.ShowInTaskbar = False
+        Me.Text = "Profile"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
