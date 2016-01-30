eb24843f197c81c5b2c5bd7f4a0275a63e2d92a7
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 22:07:55 -0500

    AddProfile()

--- src/RunThruIt/Classes/Tabs.vb
+++ src/RunThruIt/Classes/Tabs.vb
@@ -62,6 +62,18 @@
         frmManager.TabControl1.SelectedTab = t
     End Sub
 
+    Public Shared Sub AddProfile()
+        Dim t As New TabPage
+        Dim newtab As New Tab_Profile
+        newtab.Show()
+        newtab.Dock = DockStyle.Fill
+        newtab.TopLevel = False
+        t.Controls.Add(newtab)
+        frmManager.TabControl1.TabPages.Add(t)
+        newtab.Parent.Text = newtab.Text
+        frmManager.TabControl1.SelectedTab = t
+    End Sub
+
 #End Region
 
 End Class
