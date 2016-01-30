5446c448d25a150925d9660aac2f62ede714267b
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 00:22:44 -0500

    Update frmManager load event

--- src/RunThruIt/Forms/frmManager.vb
+++ src/RunThruIt/Forms/frmManager.vb
@@ -100,7 +100,11 @@
 #Region "frmManager"
 
     Private Sub frmManager_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Tabs.AddWelcome()
+        If My.Settings.set_firsttime Then
+            dlgFirstTime.ShowDialog()
+        Else
+            Tabs.AddWelcome()
+        End If
     End Sub
 
 #End Region   
