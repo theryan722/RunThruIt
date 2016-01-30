4f86a084158e463667a0f04f5b622dbffa8dccf2
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 21:29:08 -0500

    Set application icon, main form

---
+++ src/RunThruIt/Forms/frmMain.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits MetroFramework.Forms.MetroForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.MetroButton1 = New MetroFramework.Controls.MetroButton()
+        Me.SuspendLayout()
+        '
+        'MetroButton1
+        '
+        Me.MetroButton1.Location = New System.Drawing.Point(0, 0)
+        Me.MetroButton1.Name = "MetroButton1"
+        Me.MetroButton1.TabIndex = 0
+        Me.MetroButton1.UseSelectable = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(565, 368)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmMain"
+        Me.Padding = New System.Windows.Forms.Padding(0, 60, 0, 20)
+        Me.Text = "RunThruIt"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Private WithEvents MetroButton1 As MetroFramework.Controls.MetroButton
+
+End Class
