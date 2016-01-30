1e0dda610e2ae290803dff3a76e4e6b2923fba10
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 10:51:29 -0500

    Implement ConvertWorkoutTypeToColor()

--- src/RunThruIt/Forms/Tabs/Tab_Log.vb
+++ src/RunThruIt/Forms/Tabs/Tab_Log.vb
@@ -2,12 +2,39 @@
 
 #Region "Methods"
 
+    'Run
+    'Bike
+    'Walk
+    'Swim
+    'Hike
+    'Strength Training
+    'Yoga
+
     Private Sub LoadWorkouts()
 
     End Sub
 
     Private Function ConvertWorkoutTypeToColor(ByVal workouttype As String) As Color
-        
+        Dim ret As Color = Color.Green
+        Select Case workouttype
+            Case "Run"
+                ret = Color.Green
+            Case "Bike"
+                ret = Color.Orange
+            Case "Walk"
+                ret = Color.Turquoise
+            Case "Swim"
+                ret = Color.Blue
+            Case "Hike"
+                ret = Color.Red
+            Case "Strength Training"
+                ret = Color.Yellow
+            Case "Yoga"
+                ret = Color.Purple
+            Case Else
+                ret = Color.Gray
+        End Select
+        Return ret
     End Function
 
 #End Region
