e6cb4942ba4c1ebd7b1fa966275a15c3efd37bc8
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 22:44:44 -0500

    Tab_About Properties

--- src/RunThruIt/Forms/Tabs/Tab_About.Designer.vb
+++ src/RunThruIt/Forms/Tabs/Tab_About.Designer.vb
@@ -22,8 +22,18 @@ Partial Class Tab_About
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'Tab_About
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Tab_About"
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Tab_About"
+        Me.ShowInTaskbar = False
+        Me.Text = "About"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
