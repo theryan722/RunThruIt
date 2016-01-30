41fff58da5073ed42bb3dad8aea595acb2c5e5a7
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 22:05:32 -0500

    AddAbout()

--- src/RunThruIt/Classes/Tabs.vb
+++ src/RunThruIt/Classes/Tabs.vb
@@ -2,7 +2,17 @@
 
 #Region "Tabs"
 
-    
+    Public Shared Sub AddAbout()
+        Dim t As New TabPage
+        Dim newtab As New Tab_About
+        newtab.Show()
+        newtab.Dock = DockStyle.Fill
+        newtab.TopLevel = False
+        t.Controls.Add(newtab)
+        frmManager.TabControl1.TabPages.Add(t)
+        newtab.Parent.Text = newtab.Text
+        frmManager.TabControl1.SelectedTab = t
+    End Sub
 
 #End Region
 
