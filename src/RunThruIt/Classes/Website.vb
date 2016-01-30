3d44c85468ad8e8e30d6dddfbe1287d8ac1b11f2
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 17:05:34 -0500

    PostWorkoutToFeed()

--- src/RunThruIt/Classes/Website.vb
+++ src/RunThruIt/Classes/Website.vb
@@ -1,3 +1,11 @@
-﻿Public Class Website
+﻿Imports System.Net
+
+Public Class Website
+    'name|workouttype|distance|duration|sdatetime|pace|shoes
+    Public Shared Sub PostWorkoutToFeed(ByVal name As String, ByVal workouttype As String, ByVal distance As String, ByVal duration As String, ByVal sdatetime As String, ByVal pace As String, ByVal shoes As String)
+        Dim pub As String = name & "|" & workouttype & "|" & distance & "|" & duration & "|" & sdatetime & "|" & pace & "|" & shoes
+        Dim request As WebRequest = WebRequest.Create("http://runthruit.net/scripts/addworkout.php?w=" & pub)
+        request.GetResponse()
+    End Sub
 
 End Class
