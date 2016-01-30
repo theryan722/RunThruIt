edf3cdb2e468d5511338f02c9ad6527e0fb0ad1a
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 01:37:47 -0500

    RandomInfo class outline

---
+++ src/RunThruIt/Classes/RandomInfo.vb
@@ -0,0 +1,14 @@
+﻿Public Class RandomInfo
+
+    Private Shared tips() As String = {""}
+    Private Shared quotes As String = {""}
+
+    Public Shared Function GetRandomQuote() As String
+
+    End Function
+
+    Public Shared Function GetRandomTip() As String
+
+    End Function
+
+End Class
