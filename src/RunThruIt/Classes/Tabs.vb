d5c314f2c039a4daa0f05525dbe182e9192443b7
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 22:06:27 -0500

    AddInjury()

--- src/RunThruIt/Classes/Tabs.vb
+++ src/RunThruIt/Classes/Tabs.vb
@@ -26,6 +26,18 @@
         frmManager.TabControl1.SelectedTab = t
     End Sub
 
+    Public Shared Sub AddInjury()
+        Dim t As New TabPage
+        Dim newtab As New Tab_Injury
+        newtab.Show()
+        newtab.Dock = DockStyle.Fill
+        newtab.TopLevel = False
+        t.Controls.Add(newtab)
+        frmManager.TabControl1.TabPages.Add(t)
+        newtab.Parent.Text = newtab.Text
+        frmManager.TabControl1.SelectedTab = t
+    End Sub
+
 #End Region
 
 End Class
