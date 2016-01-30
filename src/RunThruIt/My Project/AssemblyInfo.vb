c5bf44c335063136e99379b90ffc86854f5cfc55
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 23:14:14 -0500

    Set assembly information and publish options

--- src/RunThruIt/My Project/AssemblyInfo.vb
+++ src/RunThruIt/My Project/AssemblyInfo.vb
@@ -10,9 +10,9 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyTitle("RunThruIt")> 
 <Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyCompany("Ryan O'Day")> 
 <Assembly: AssemblyProduct("RunThruIt")> 
-<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016 Ryan O'Day")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
