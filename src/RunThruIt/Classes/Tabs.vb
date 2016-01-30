863d04224ea428f992cd72f3723bee38d6f1e078
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 22:06:50 -0500

    AddLog()

--- src/RunThruIt/Classes/Tabs.vb
+++ src/RunThruIt/Classes/Tabs.vb
@@ -38,6 +38,18 @@
         frmManager.TabControl1.SelectedTab = t
     End Sub
 
+    Public Shared Sub AddLog()
+        Dim t As New TabPage
+        Dim newtab As New Tab_Log
+        newtab.Show()
+        newtab.Dock = DockStyle.Fill
+        newtab.TopLevel = False
+        t.Controls.Add(newtab)
+        frmManager.TabControl1.TabPages.Add(t)
+        newtab.Parent.Text = newtab.Text
+        frmManager.TabControl1.SelectedTab = t
+    End Sub
+
 #End Region
 
 End Class
