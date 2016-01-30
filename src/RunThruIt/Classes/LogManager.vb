a66a9e453663ba09c1f70e47cad0b1eeca10e3e5
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 09:56:01 -0500

    Outline LogManager code

--- src/RunThruIt/Classes/LogManager.vb
+++ src/RunThruIt/Classes/LogManager.vb
@@ -1,14 +1,31 @@
 ﻿Public Class LogManager
 
-#Region "Write"
+#Region "Public"
+
+    Public Shared Sub AddToLog(ByVal nworkout As Workout)
+
+    End Sub
+
+    Public Shared Sub RemoveFromLog(ByVal nworkout As Workout)
+
+    End Sub
+
+    Public Shared Function GetWorkouts() As List(Of Workout)
+
+    End Function
 
 
+    Public Shared Sub ClearWorkouts()
+
+    End Sub
 
 #End Region
 
-#Region "Read"
+#Region "Private"
 
+    Private Function ParseSetting(ByVal str As String) As Workout
 
+    End Function
 
 #End Region
 
