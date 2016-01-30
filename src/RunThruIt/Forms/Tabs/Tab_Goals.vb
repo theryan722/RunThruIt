458f9908427d60573fba3c97a45ba95de670a984
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 11:34:32 -0500

    Outline code for Tab_Goals

--- src/RunThruIt/Forms/Tabs/Tab_Goals.vb
+++ src/RunThruIt/Forms/Tabs/Tab_Goals.vb
@@ -22,18 +22,66 @@
 
 #End Region
 
+    Private Sub btnReload_Click(sender As Object, e As EventArgs) Handles btnReload.Click
+
+    End Sub
+
+    Private Sub btnRemoveAll_Click(sender As Object, e As EventArgs) Handles btnRemoveAll.Click
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+    End Sub
+
 #End Region
 
 #Region "Methods"
 
+    Private Sub LoadGoals()
 
+    End Sub
+
+    Private Sub LoadCompleted()
+
+    End Sub
+
+    Private Sub LoadAll()
+
+    End Sub
+
+    Private Sub MarkGoalAsCompleted()
+
+    End Sub
+
+    Private Sub MarkGoalAsIncomplete()
+
+    End Sub
+
+    Private Sub RemoveGoal()
+
+    End Sub
+
+    Private Sub RemoveCompleted()
+
+    End Sub
+
+    Private Sub RemoveAllCompleted()
+
+    End Sub
+
+    Private Sub RemoveAllGoals()
+
+    End Sub
 
 #End Region
 
 #Region "Tab_Goals"
 
+    Private Sub Tab_Goals_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
 
 #End Region
-
+    
 End Class
