0b43f15a725afbd9b29b3192ec7a516808321fe2
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 10:24:13 -0500

    Implement btnRemove, add dlgViewLog

--- src/RunThruIt/Classes/Controls/ctrl_logitem.vb
+++ src/RunThruIt/Classes/Controls/ctrl_logitem.vb
@@ -5,7 +5,10 @@
 #Region "Buttons"
 
     Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
-
+        If MetroFramework.MetroMessageBox.Show(frmManager, "Are you sure you want to remove this workout?", "Remove Workout", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+            LogManager.RemoveFromLog(WorkoutItem)
+            Me.Dispose()
+        End If
     End Sub
 
     Private Sub btnView_Click(sender As Object, e As EventArgs) Handles btnView.Click
