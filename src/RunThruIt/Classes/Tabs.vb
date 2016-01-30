7358ce88a944d42e111aa238f4db43c75912062b
Ryan O'Day <romanguy722@gmail.com>
2016-01-29 22:07:20 -0500

    AddOptions()

--- src/RunThruIt/Classes/Tabs.vb
+++ src/RunThruIt/Classes/Tabs.vb
@@ -50,6 +50,18 @@
         frmManager.TabControl1.SelectedTab = t
     End Sub
 
+    Public Shared Sub AddOptions()
+        Dim t As New TabPage
+        Dim newtab As New Tab_Options
+        newtab.Show()
+        newtab.Dock = DockStyle.Fill
+        newtab.TopLevel = False
+        t.Controls.Add(newtab)
+        frmManager.TabControl1.TabPages.Add(t)
+        newtab.Parent.Text = newtab.Text
+        frmManager.TabControl1.SelectedTab = t
+    End Sub
+
 #End Region
 
 End Class
