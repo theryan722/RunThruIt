d2d35e9e60a1ff2160d7816db2c932b86f14b0a1
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 12:51:55 -0500

    Add constructor to shoe

--- src/RunThruIt/Classes/Objects/Shoe.vb
+++ src/RunThruIt/Classes/Objects/Shoe.vb
@@ -4,4 +4,18 @@
     Public Property Mileage As String
     Public Property Notes As String
 
+#Region "Constructor"
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(ByVal sname As String, ByVal smileage As String, ByVal snotes As String)
+        ShoeName = sname
+        Mileage = smileage
+        Notes = snotes
+    End Sub
+
+#End Region
+
 End Class
