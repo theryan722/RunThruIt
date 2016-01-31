282310c836999853c224a622f5895a1c5c5efbeb
Ryan O'Day <romanguy722@gmail.com>
2016-01-30 23:54:03 -0500

    Water Portion of other trackers

--- src/RunThruIt/Forms/Tabs/Tab_OtherTrackers.vb
+++ src/RunThruIt/Forms/Tabs/Tab_OtherTrackers.vb
@@ -5,16 +5,27 @@
 #Region "UI"
 
     Private Sub btn_water_addintakefortoday_Click(sender As Object, e As EventArgs) Handles btn_water_addintakefortoday.Click
-
+        Dim wat As String = InputBox("Please enter the amount of water you have consumed today, in fl.oz.", "Enter Water Intake", "0")
+        My.Settings.log_water.Add(DateTime.Today.ToString("M/dd/yyyy") & "|" & wat & " oz")
+        LoadWater()
     End Sub
 
     Private Sub btn_water_removeselected_Click(sender As Object, e As EventArgs) Handles btn_water_removeselected.Click
-
+        If lb_water.SelectedIndex <> -1 Then
+            My.Settings.log_water.Remove(lb_water.SelectedItem)
+            LoadWater()
+        End If
     End Sub
 
 #End Region
 
 #Region "Methods"
+
+    Private Sub CheckWaterIntake(ByVal wat As Integer)
+        If wat < 40 Then
+            MetroFramework.MetroMessageBox.Show(frmManager, "Notice: Your water consumption for today was low. Please consider increasing your water intake.", "Water Intake Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
 
     Private Sub LoadWater()
         lb_water.Items.Clear()
